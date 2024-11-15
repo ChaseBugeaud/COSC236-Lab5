c48bb3794142dc083240f53788e79b573c1704b5
Himeeka <Goodlandliam@gmail.com>
2024-11-15 15:47:15 -0800

    Added book interface, changed Book class to PaperBook, Changed member so that it accessed Book interface methods

---
+++ src/lab5/Book.java
@@ -0,0 +1,12 @@
+package lab5;
+
+public interface Book {
+	
+	public String toString();
+	
+	public void setIsAvailable(boolean isAvailable);
+	
+	public boolean getIsAvailable();
+	
+	public String getTitle();
+}
