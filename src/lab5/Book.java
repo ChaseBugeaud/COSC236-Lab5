5a95a38cfe448f1674c0d0f6b892a144187b9ad3
Chase Bugeaud <cbugeaud@shaw.ca>
2024-11-15 16:03:01 -0800

    created Ebook, AudioBook, RareBook classes

--- src/lab5/Book.java
+++ src/lab5/Book.java
@@ -1,12 +1,12 @@
 package lab5;
 
 public interface Book {
-	
-	public String toString();
-	
-	public void setIsAvailable(boolean isAvailable);
-	
-	public boolean getIsAvailable();
-	
-	public String getTitle();
+
+  public String toString();
+
+  public void setIsAvailable(boolean isAvailable);
+
+  public boolean getIsAvailable();
+
+  public String getTitle();
 }
