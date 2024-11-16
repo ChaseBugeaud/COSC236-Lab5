80ce4c6c26c60ac1e7c24af7049d7387f31c90f0
KIverson11 <kiverson11@gmail.com>
2024-11-15 16:25:58 -0800

    paperBook test added

---
+++ src/lab5/tests/TestPaperBook.java
@@ -0,0 +1,23 @@
+package lab5.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import lab5.Book;
+import lab5.PaperBook;
+
+public class TestPaperBook {
+
+	@Test
+	void testObjectName() {
+		Book book = new PaperBook("harry Potter");
+		
+		
+		assertEquals(book.getClass().getName(), "lab5.PaperBook");
+		assertTrue(book instanceof lab5.Book);
+	}
+	
+	
+}
