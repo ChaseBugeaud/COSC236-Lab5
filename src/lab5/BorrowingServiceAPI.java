59659fe451dee91e1d02db901c5bc8b3e538ad27
KIverson11 <kiverson11@gmail.com>
2024-11-22 17:07:28 -0800

    changes

--- src/lab5/BorrowingServiceAPI.java
+++ src/lab5/BorrowingServiceAPI.java
@@ -3,7 +3,9 @@ package lab5;
 import java.util.ArrayList;
 
 public interface BorrowingServiceAPI {
-	public void borrowBook(Book book,ArrayList<Book> arrBooks);
+	public void borrowBook(Book book, ArrayList<Book> arrBooks);
+	
+	public void returnAllBorrowedBooks(Book book, ArrayList<Book> arrBooks);
 	
 	public void returnBook(Book book, ArrayList<Book> arrBooks); 
 }
