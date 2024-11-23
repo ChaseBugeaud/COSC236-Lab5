30fa1c2c049200b0b43d9138b213ba34909b5fd5
ChaseBugeaud <cbugeaud@shaw.ca>
2024-11-22 16:39:55 -0800

    formatting borrowingservices.java

--- src/lab5/BorrowingServices.java
+++ src/lab5/BorrowingServices.java
@@ -3,22 +3,20 @@ package lab5;
 import java.util.ArrayList;
 
 public class BorrowingServices implements BorrowingServiceAPI {
-	
-	
-	@Override
-	public void borrowBook(Book book, ArrayList<Book> arrBooks) {
-		if (book != null && book.getIsAvailable() == true) {
-			arrBooks.add(book);
-			book.setIsAvailable(false);
-		}
-	}
-	@Override
-	public void returnBook(Book book, ArrayList<Book> arrBooks) {
-		if (book != null) {
-			arrBooks.remove(book);
-			book.setIsAvailable(true);
-		}
-	}
-	
 
+  @Override
+  public void borrowBook(Book book, ArrayList<Book> arrBooks) {
+    if (book != null && book.getIsAvailable() == true) {
+      arrBooks.add(book);
+      book.setIsAvailable(false);
+    }
+  }
+
+  @Override
+  public void returnBook(Book book, ArrayList<Book> arrBooks) {
+    if (book != null) {
+      arrBooks.remove(book);
+      book.setIsAvailable(true);
+    }
+  }
 }
