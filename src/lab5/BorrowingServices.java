ac40b15735a3df49a945a84a1620ed0dea1f6e01
KIverson11 <kiverson11@gmail.com>
2024-11-22 16:51:44 -0800

    chase says hoes commit

--- src/lab5/BorrowingServices.java
+++ src/lab5/BorrowingServices.java
@@ -1,22 +1,34 @@
 package lab5;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class BorrowingServices implements BorrowingServiceAPI {
+	
+	
+	@Override
+	public void borrowBook(Book book, ArrayList<Book> arrBooks) {
+		if (book != null && book.getIsAvailable() == true) {
+			arrBooks.add(book);
+			book.setIsAvailable(false);
+		}
+	}
+	@Override
+	public void returnBook(Book book, ArrayList<Book> arrBooks) {
+		if (book != null) {
+			arrBooks.remove(book);
+			book.setIsAvailable(true);
+		}
+	}
+	@Override
+	public void returnAllBorrowedBooks(Book book, ArrayList<Book> arrBooks) {
+		Iterator<Book> bookIterator = arrBooks.iterator();
+	    while(bookIterator.hasNext()) {
+		   	 book = bookIterator.next();
+		   	 book.setIsAvailable(true);
+	    }
+	    arrBooks.clear(); // clear array of borrowed books
+	}
 
-  @Override
-  public void borrowBook(Book book, ArrayList<Book> arrBooks) {
-    if (book != null && book.getIsAvailable() == true) {
-      arrBooks.add(book);
-      book.setIsAvailable(false);
-    }
-  }
 
-  @Override
-  public void returnBook(Book book, ArrayList<Book> arrBooks) {
-    if (book != null) {
-      arrBooks.remove(book);
-      book.setIsAvailable(true);
-    }
-  }
 }
