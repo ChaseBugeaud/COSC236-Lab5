b549e6364b14d53f2c105cf62b64ab0e3ffd1237
KIverson11 <kiverson11@gmail.com>
2024-11-22 16:51:00 -0800

    chase says hoes commit

--- src/lab5/Member.java
+++ src/lab5/Member.java
@@ -4,62 +4,48 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Member {
-
-  private String name;
-  private ArrayList<Book> borrowedBooks; // Book class dependency
-
-  public Member(String name) {
-    this.name = name;
-    this.borrowedBooks = new ArrayList<>();
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public ArrayList<Book> getBorrowedBooks() {
-    return borrowedBooks;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String toString() {
-    return "Member: " + name;
-  }
-
-  public void borrowBook(Book book) {
-    BorrowingServices borrowService = new BorrowingServices();
-    if (book != null && book.getIsAvailable() == true) {
-      borrowService.borrowBook(book, borrowedBooks);
-      book.setIsAvailable(false);
-    }
-  }
-
-  public void returnBook(Book book) {
-    BorrowingServices borrowService = new BorrowingServices();
-    if (book != null) {
-      borrowService.returnBook(book, borrowedBooks);
-      book.setIsAvailable(true);
-    }
-  }
-
-  public void listBorrowedBooks() {
-    for (Book book : borrowedBooks)
-      System.out.println(book); // book.toString()
-  }
-
-  public int borrowedBooksCount() {
-    return borrowedBooks.size();
-  }
-
-  public void returnAllBooks() {
-    Iterator<Book> bookIterator = borrowedBooks.iterator();
-    while (bookIterator.hasNext()) {
-      Book book = bookIterator.next();
-      book.setIsAvailable(true);
-    }
-    borrowedBooks.clear(); // clear array of borrowed books
-  }
+	
+	private String name;
+	private ArrayList<Book> borrowedBooks; // Book class dependency
+	
+	
+	public Member(String name) {
+		this.name = name;
+		this.borrowedBooks = new ArrayList<>();
+	}
+	public String getName() {
+		return name;
+	}
+	public ArrayList<Book> getBorrowedBooks() { 
+		return borrowedBooks;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String toString() {
+		return "Member: " + name;
+	}
+	public void borrowBook(Book book) {
+		BorrowingServices borrowService = new BorrowingServices();
+		borrowService.borrowBook(book,borrowedBooks);
+		
+	}
+	public void returnBook(Book book) {
+	BorrowingServices borrowService = new BorrowingServices();
+	borrowService.returnBook(book,borrowedBooks);
+	}
+	
+	public void listBorrowedBooks() {
+		for (Book book : borrowedBooks)
+			System.out.println(book); 
+	}
+	
+	public int borrowedBooksCount() {
+		return borrowedBooks.size();
+	}
+	
+	public void returnAllBooks(Book book) {
+		BorrowingServices borrowService = new BorrowingServices();
+		borrowService.returnAllBooks(book, borrowedBooks);
+	}
 }
