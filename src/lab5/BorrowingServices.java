20a44940da67a2382120c84858a54ae90575e7d3
ChaseBugeaud <cbugeaud@shaw.ca>
2024-11-29 12:43:33 -0800

    BorrowingService checks for book availability

--- src/lab5/BorrowingServices.java
+++ src/lab5/BorrowingServices.java
@@ -4,35 +4,33 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class BorrowingServices implements BorrowingServiceAPI {
-	
-	
-	@Override
-	public void borrowBook(Book book, ArrayList<Book> arrBooks) {
-		if (book != null && book.getIsAvailable() == true) {
-			arrBooks.add(book);
-			book.setIsAvailable(false);
-		}
-	}
-	@Override
-	public void returnBook(Book book, ArrayList<Book> arrBooks) {
-		if (book != null) {
-			arrBooks.remove(book);
-			book.setIsAvailable(true);
-		}
-	}
-	@Override
-	public void returnAllBorrowedBooks(ArrayList<Book> borrowedBooks) {
-		Book book = borrowedBooks.get(0);
-		Iterator<Book> bookIterator = borrowedBooks.iterator();
-		while(bookIterator.hasNext()) {
-			book.setIsAvailable(true);
-			book = bookIterator.next();
-		}
-		System.out.println("All books cleared");
-		
-	    borrowedBooks.clear();
-		
-	}
 
+  @Override
+  public void borrowBook(Book book, ArrayList<Book> arrBooks) {
+    if (book != null && book.getIsAvailable()) {
+      arrBooks.add(book);
+      book.setIsAvailable(false);
+    }
+  }
+
+  @Override
+  public void returnBook(Book book, ArrayList<Book> arrBooks) {
+    if (book != null && !book.getIsAvailable()) {
+      arrBooks.remove(book);
+      book.setIsAvailable(true);
+    }
+  }
+
+  @Override
+  public void returnAllBorrowedBooks(ArrayList<Book> borrowedBooks) {
+    Book book = borrowedBooks.get(0);
+    Iterator<Book> bookIterator = borrowedBooks.iterator();
+    while (bookIterator.hasNext()) {
+      book.setIsAvailable(true);
+      book = bookIterator.next();
+    }
+    System.out.println("All books cleared");
+    borrowedBooks.clear();
+  }
 
 }
--- src/lab5/Member.java
+++ src/lab5/Member.java
@@ -4,48 +4,53 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Member {
-	
-	private String name;
-	private ArrayList<Book> borrowedBooks; // Book class dependency
-	
-	
-	public Member(String name) {
-		this.name = name;
-		this.borrowedBooks = new ArrayList<>();
-	}
-	public String getName() {
-		return name;
-	}
-	public ArrayList<Book> getBorrowedBooks() { 
-		return borrowedBooks;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String toString() {
-		return "Member: " + name;
-	}
-	public void borrowBook(Book book) {
-		BorrowingServices borrowService = new BorrowingServices();
-		borrowService.borrowBook(book,borrowedBooks);
-		
-	}
-	public void returnBook(Book book) {
-	BorrowingServices borrowService = new BorrowingServices();
-	borrowService.returnBook(book,borrowedBooks);
-	}
-	
-	public void listBorrowedBooks() {
-		for (Book book : borrowedBooks)
-			System.out.println(book); 
-	}
-	
-	public int borrowedBooksCount() {
-		return borrowedBooks.size();
-	}
-	
-	public void returnAllBooks() {
-		BorrowingServices borrowService = new BorrowingServices();
-		borrowService.returnAllBorrowedBooks(borrowedBooks);
-	}
+
+  private String name;
+  private ArrayList<Book> borrowedBooks; // Book class dependency
+
+  public Member(String name) {
+    this.name = name;
+    this.borrowedBooks = new ArrayList<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public ArrayList<Book> getBorrowedBooks() {
+    return borrowedBooks;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String toString() {
+    return "Member: " + name;
+  }
+
+  public void borrowBook(Book book) {
+    BorrowingServices borrowService = new BorrowingServices();
+    borrowService.borrowBook(book, borrowedBooks);
+
+  }
+
+  public void returnBook(Book book) {
+    BorrowingServices borrowService = new BorrowingServices();
+    borrowService.returnBook(book, borrowedBooks);
+  }
+
+  public void listBorrowedBooks() {
+    for (Book book : borrowedBooks)
+      System.out.println(book);
+  }
+
+  public int borrowedBooksCount() {
+    return borrowedBooks.size();
+  }
+
+  public void returnAllBooks() {
+    BorrowingServices borrowService = new BorrowingServices();
+    borrowService.returnAllBorrowedBooks(borrowedBooks);
+  }
 }
